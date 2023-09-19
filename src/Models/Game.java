7146b348c351a072cb1d2becd055a9ebbc96ccf8
Elkin Angulo <elkinsamir643@gmail.com>
2023-09-19 10:21:41 -0500

    Modelo Game creado

---
+++ src/Models/Game.java
@@ -0,0 +1,67 @@
+package Models;
+
+public class Game 
+{
+    
+    //! Declaración de variables sujeta a cambios
+    private static char[] operadores = {'+', '-', '*', '%'};
+    private char operador; 
+    private int numeroUno, numeroDos, resultado;
+    private int aciertos, fallos; 
+
+    public void obtenerOperaciónAleatoria(int min, int max)
+    {
+        numeroUno = (int)(Math.random() * max) + min;        
+        numeroDos = (int)(Math.random() * max) + min;
+        operador = operadores[(int)(Math.random() * operadores.length)];
+    }
+
+    public int obtenerResultado()
+    {
+        switch (operador) 
+        {
+            case '+':
+                resultado = numeroUno + numeroDos;
+                break;
+            case '-':
+                resultado = numeroUno - numeroDos;
+                break;
+            case '*':
+                resultado = numeroUno * numeroDos;
+                break;
+            case '%': 
+                resultado = numeroUno % numeroDos;
+                break;
+            default:
+                throw new Error("Operador no encontrado", null);
+        }
+        return resultado;
+    }
+
+
+    public int getNumeroUno() {
+        return this.numeroUno;
+    }
+
+    public int getNumeroDos() {
+        return this.numeroDos;
+    }
+
+    public int getResultado() {
+        return this.resultado;
+    }
+
+    public int getAciertos() {
+        return this.aciertos;
+    }
+
+    public int getFallos() {
+        return this.fallos;
+    }
+
+    public char getOperador() {
+        return this.operador;
+    }
+}
+
+
